e0e17fe686cc3232a4f16a542a618e269ca6fa9c
Ronin Gomez <rgomez9@uncc.edu>
2021-01-28 11:00:50 -0500

    Testing file

---
+++ testing.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 10:59:07 2021
+
+@author: rongomez
+"""
+
+def testing():
+    print("Hello")
+    
+    
+def main():
+    testing()
+
+if __name__ == "__main__":
+    main() 
